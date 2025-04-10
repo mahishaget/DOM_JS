e157b5c468b5a85b3a0d26003e0dcf7c97c5443c
mahishaget <mahletg738@gmail.com>
2025-04-10 19:54:17 +0300

    last commmit

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ const overlay=document.querySelector('.overlay');
 const btnCloseModel=document.querySelector('.close-modal');
 const btnsOpenModel=document.querySelectorAll('.show_modal');
 console.log(btnsOpenModel);
+const openModal=function(){
+     console.log('Button clicked'); 
+     modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');}   
+const closeModal=function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');};
 for(let i=0;i<btnsOpenModel.length;i++)
-btnsOpenModel[i].addEventListener('click',function( )
-{
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
+btnsOpenModel[i].addEventListener('click',openModal )   
+ // modal.style.display='block';
+btnCloseModel.addEventListener('click',closeModal);
 
-});
+overlay.addEventListener('click',closeModal);
+    // modal.classList.add('hidden');
+    // overlay.classList.add('hidden');
 
